4b4e586bbcc2a94f3179d8b9877c27bb61eee2e3
shinarasheed <rasheedshinaopeyemi@gmail.com>
2021-11-07 16:04:09 +0100

    updated update van endpoint

--- src/components/OrderFooter.js
+++ src/components/OrderFooter.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { useDispatch } from "react-redux";
 import { StyleSheet, View, TouchableOpacity, Text } from "react-native";
 import { useNavigation } from "@react-navigation/native";
